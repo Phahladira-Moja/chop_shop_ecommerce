69b65609f44861c2460cec80abc93cf39ce9d26e
Phahladira-Moja <phahladira@patala.co.za>
2023-06-13 16:08:11 +0200

    Created the Navbar component with styles, also aded Commerce.js file

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -16,7 +16,11 @@ const Product = ({ product }) => {
 
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} image="" title={product.name} />
+      <CardMedia
+        className={classes.media}
+        image={product.image}
+        title={product.name}
+      />
       <CardContent>
         <div className={classes.cardContent}>
           <Typography variant="h5" gutterBottom>
@@ -25,7 +29,7 @@ const Product = ({ product }) => {
           <Typography variant="h5">{product.price}</Typography>
         </div>
 
-        <Typography variant="h2" color="textSecondary">
+        <Typography variant="body2" color="textSecondary">
           {product.description}
         </Typography>
       </CardContent>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import Grid from "@material-ui/core";
-import Product from "./Products";
+import { Grid } from "@material-ui/core";
+import Product from "./Product/Product";
+
+import useStyles from "./styles";
 
 const products = [
   {
@@ -8,18 +10,25 @@ const products = [
     name: "Shoes",
     description: "Running shoes",
     price: "$5",
+    image:
+      "https://cdn.shopify.com/s/files/1/0267/2315/6143/products/s7.3024883-400_DEFAULT.png?v=1678709991&width=640&height=800&crop=center",
   },
   {
     id: 2,
     name: "Macbook",
     description: "Apple Macbook",
     price: "$10",
+    image:
+      "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTJ2LoD9GuxpyuMZlohX-eOuUlH62s1CO7whPJ9XVGKtDYbzARpzcSc4qatwDxBJ1kXoEin3E6cGwmZDPw9MekaK7Q4LKtFbmBhIVglUQLmPHLbeC0Z9GDlAQ&usqp=CAE",
   },
 ];
 
 const Products = () => {
+  const classes = useStyles();
+
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
